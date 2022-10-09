e1ae66075deda04f4eed213dd65a1ad5bb03d3b5
Thurn4X <113912459+Thurn4X@users.noreply.github.com>
2022-10-09 16:07:25 +0200

    Add files via upload

---
+++ javascripthehe.js
@@ -0,0 +1,11 @@
+
+function changeimage(sexe){
+    let test = sexe
+    if (test == "homme"){
+        document.getElementById("image").src = "sacdefrappe.jpg";
+    } 
+    if (test == "femme"){
+        document.getElementById("image").src = "gants.jpeg";
+    }
+
+}
