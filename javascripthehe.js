24f55cd3f8e85a9f9f539faccda52ce3a851cdcb
Thurn4X <113912459+Thurn4X@users.noreply.github.com>
2022-10-10 17:39:02 +0200

    Add files via upload

--- javascripthehe.js
+++ javascripthehe.js
@@ -1,11 +1,83 @@
+var sexe, ethnie;
+let produit_id = new URLSearchParams(window.location.search).get("id")
+if (produit_id == 1){
+    document.getElementById("image").src = "gants.jpeg";
+}else if (produit_id == 2){
+    document.getElementById("image").src = "suuuu.jpg";
+}else if (produit_id == 3){
+    document.getElementById("image").src = "pb.jpg";
+}
 
-function changeimage(sexe){
-    let test = sexe
-    if (test == "homme"){
-        document.getElementById("image").src = "sacdefrappe.jpg";
-    } 
-    if (test == "femme"){
-        document.getElementById("image").src = "gants.jpeg";
+class Produits{
+    constructor(type, taille, couleur) {
+    this._typeproduit = type
+    this._taille = taille
+    this._couleur = couleur
+    
     }
 
+    }
+
+function changesexe(jojo){
+    sexe = jojo
+        if (sexe == "rien"){
+            document.getElementById("image").src = "gants.jpeg";
+        }
+        if (sexe == "homme" && ethnie == "arabe"){
+            document.getElementById("image").src = "photos/yamenmalepb.png";
+            console.log("tu as appuyé sur homme et arabe etais selectionné")
+        }
+        if (sexe == "femme" && ethnie == "mexicain"){
+            document.getElementById("image").src = "photos/melfemalepb.png";
+            console.log("tu as appuyé sur femme et mexicain etais selectionné")
+        }
+        if (sexe == "homme" && ethnie == "russe"){
+            document.getElementById("image").src = "photos/ruskimalepb.png";
+            console.log(sexe, ethnie)
+        }
+        if (sexe == "homme" && ethnie == "ptitarabe"){
+            document.getElementById("image").src = "photos/anasmalepb.png";
+        }
+        if (sexe == "homme" && ethnie == "pakistanais"){
+            document.getElementById("image").src = "photos/yamenmale.png";
+        }
+        if (sexe == "homme" && ethnie == "libanais"){
+            document.getElementById("image").src = "photos/yamenmale.png";
+        }
+        if (sexe == "homme" && ethnie == "singe"){
+            document.getElementById("image").src = "photos/paomalepb.png";
+        }      
+}
+
+
+
+function changeimage(yeboy){
+    ethnie = yeboy
+    if (ethnie == "rien"){
+        document.getElementById("image").src = "gants.jpeg";
+    }
+    if (sexe == "homme" && ethnie == "arabe"){
+        document.getElementById("image").src = "photos/yamenmalepb.png";
+        console.log("tu as appuyé sur homme et arabe etais selectionné")
+    }
+    if (sexe == "femme" && ethnie == "mexicain"){
+        document.getElementById("image").src = "photos/melfemalepb.png";
+        console.log("tu as appuyé sur femme et mexicain etais selectionné")
+    }
+    if (sexe == "homme" && ethnie == "russe"){
+        document.getElementById("image").src = "photos/ruskimalepb.png";
+        console.log(sexe, ethnie)
+    }
+    if (sexe == "homme" && ethnie == "ptitarabe"){
+        document.getElementById("image").src = "photos/anasmalepb.png";
+    }
+    if (sexe == "homme" && ethnie == "pakistanais"){
+        document.getElementById("image").src = "photos/yamenmale.png";
+    }
+    if (sexe == "homme" && ethnie == "libanais"){
+        document.getElementById("image").src = "photos/yamenmale.png";
+    }
+    if (sexe == "homme" && ethnie == "singe"){
+        document.getElementById("image").src = "photos/paomalepb.png";
+    }      
 }
